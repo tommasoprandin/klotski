03fb18e6936f6a22db415741701c95a53564b099
EliaDamo <132848235+EliaDamo@users.noreply.github.com>
2023-06-02 18:17:21 +0200

    Update Configuration.java

--- src/main/java/it/unipd/models/Configuration.java
+++ src/main/java/it/unipd/models/Configuration.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class Configuration {
     public final static int BLOCKS_NUM = 10;
-    private final Block[] blocks = new Block[BLOCKS_NUM];
+    private Block[] blocks = new Block[BLOCKS_NUM];
+
+    private int goalBlockIdx;
 
     public Configuration(int type) {
         switch (type) {
@@ -19,6 +21,7 @@ public class Configuration {
                 blocks[7] = new Block(1, 3, 1, 1);
                 blocks[8] = new Block(2, 3, 1, 1);
                 blocks[9] = new Block(1, 4, 2, 1);
+                goalBlockIdx = 0;
                 break;
             }
             case 1:
@@ -32,6 +35,7 @@ public class Configuration {
                 blocks[7] = new Block(3, 3, 1, 1);
                 blocks[8] = new Block(0, 4, 2, 1);
                 blocks[9] = new Block(2, 4, 2, 1);
+                goalBlockIdx = 0;
                 break;
             case 2:
                 blocks[0] = new Block(2, 1, 2, 2);
@@ -44,6 +48,7 @@ public class Configuration {
                 blocks[7] = new Block(1, 3, 2, 1);
                 blocks[8] = new Block(3, 3, 1, 1);
                 blocks[9] = new Block(2, 4, 2, 1);
+                goalBlockIdx = 0;
                 break;
             case 3:
                 blocks[0] = new Block(1, 0, 2, 2);
@@ -56,10 +61,16 @@ public class Configuration {
                 blocks[7] = new Block(2, 3, 1, 1);
                 blocks[8] = new Block(0, 4, 1, 1);
                 blocks[9] = new Block(3, 4, 1, 1);
+                goalBlockIdx = 0;
                 break;
             default:
                 break;
         }
+    }
+
+    public Configuration(Block[] blocks){
+        this.blocks=blocks;
+        goalBlockIdx=0;
     }
 
     public Block[] getBlocks() {
@@ -68,6 +79,10 @@ public class Configuration {
             copy[i] = new Block(blocks[i]);
         }
         return copy;
+    }
+
+    public int getGoalBlockIdx() {
+        return goalBlockIdx;
     }
 
     @Override
