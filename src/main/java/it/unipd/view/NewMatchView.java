28aab4614947a67e44fb490f9f683af98ecc5e13
EliaDamo <132848235+EliaDamo@users.noreply.github.com>
2023-06-02 18:20:40 +0200

    Update NewMatchView.java

--- src/main/java/it/unipd/view/NewMatchView.java
+++ src/main/java/it/unipd/view/NewMatchView.java
@@ -6,8 +6,12 @@ import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class NewMatchView extends View {
+
+public class NewMatchView extends Pane implements View {
     private final MatchController controller;
     private final VBox container;
     private final ToggleGroup configGrp;
@@ -15,6 +19,8 @@ public class NewMatchView extends View {
     private final TextField usernameTxt;
     private final PasswordField passwordTxt;
     private final Button startBtn;
+
+    private final Button resumeBtn;
 
     public NewMatchView() {
         controller = MatchController.getInstance();
@@ -30,14 +36,29 @@ public class NewMatchView extends View {
         startBtn = new Button("Inizia");
         startBtn.setOnMouseClicked((evt) -> {
             int config = 0;
-            for (int i = 0; i < configBtns.length; i++) {
+            int i;
+            for (i = 0; i < configBtns.length; i++) {
                if (configBtns[i].equals(configGrp.getSelectedToggle())) config = i;
             }
             controller.createNewMatch(new Configuration(config), usernameTxt.getText(), passwordTxt.getText());
         });
+        resumeBtn = new Button("Riprendi");
+        resumeBtn.setOnMouseClicked((evt) -> {
+            try {
+                File readFile = new File("movefile.txt");
+                Scanner in = new Scanner(readFile);
+                String s = in.nextLine();
+                String[] input = s.split(" ");
+                String u = input[0];
+                String p= input[1];
+                in.close();
+                controller.resumeMatch(u, p);
+            }catch(IOException e) {}
+
+        });
         container = new VBox();
         container.getChildren().addAll(configBtns);
-        container.getChildren().addAll(usernameTxt, passwordTxt, startBtn);
+        container.getChildren().addAll(usernameTxt, passwordTxt, startBtn, resumeBtn);
         this.getChildren().add(container);
         this.setStyle("-fx-font-family: sans-serif");
     }
@@ -45,5 +66,15 @@ public class NewMatchView extends View {
     @Override
     public void render(Object data) {
 
+    }
+
+    @Override
+    public void hide() {
+        super.setVisible(false);
+    }
+
+    @Override
+    public void show() {
+        super.setVisible(true);
     }
 }
