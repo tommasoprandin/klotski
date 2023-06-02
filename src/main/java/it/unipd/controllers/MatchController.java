31c89a968c66919b87ff8522f69d02fb64d3e425
EliaDamo <132848235+EliaDamo@users.noreply.github.com>
2023-06-02 18:15:20 +0200

    Update MatchController.java

--- src/main/java/it/unipd/controllers/MatchController.java
+++ src/main/java/it/unipd/controllers/MatchController.java
@@ -1,26 +1,24 @@
 package it.unipd.controllers;
 
 import it.unipd.models.*;
-import it.unipd.utils.BestMoveFetcher;
 import it.unipd.view.MatchView;
 import it.unipd.view.NewMatchView;
-import it.unipd.view.View;
+import java.nio.*;
+import java.io.*;
+import java.util.Stack;
+import java.util.*;
 
-import java.net.URI;
-
-public class MatchController extends Controller {
+public class MatchController implements Observer {
     private Match match;
     private Configuration config;
     private Block selBlock;
-    private View newMatchView;
-    private View matchView;
-    private View winView;
-    private BestMoveFetcher bestMoveFetcher;
+    private NewMatchView newMatchView;
+    private MatchView matchView;
+    private FileWriter moveFile;
 
     private static MatchController instance;
 
     private MatchController() {
-        bestMoveFetcher = new BestMoveFetcher(URI.create("http://localhost:9000/2015-03-31/functions/function/invocations"));
     }
     public static MatchController getInstance() {
         if (instance == null) {
@@ -30,42 +28,71 @@ public class MatchController extends Controller {
         return instance;
     }
 
-    public View getNewMatchView() {
+    public NewMatchView getNewMatchView() {
         return newMatchView;
     }
 
-    public View getMatchView() {
+    public MatchView getMatchView() {
         return matchView;
     }
 
-    public View getWinView() {
-        return winView;
-    }
-
-    public void setNewMatchView(View nmv) {
+    public void setNewMatchView(NewMatchView nmv) {
         newMatchView = nmv;
     }
 
-    public void setMatchView(View matchView) {
+    public void setMatchView(MatchView matchView) {
         this.matchView = matchView;
-    }
-
-    public void setWinView(View winView) {
-        this.winView = winView;
     }
 
     public void start() {
         newMatchView.show();
         matchView.hide();
-        winView.hide();
     }
 
     public void createNewMatch(Configuration config, String username, String password) {
-        match = new Match(new User(username, password), new Board(5, 4, config.getBlocks()));
+        match = new Match(new User(username, password), new Board(5, 4, config.getGoalBlockIdx(), config.getBlocks()));
         this.config = config;
         newMatchView.hide();
         matchView.render(match);
         matchView.show();
+        try {
+            moveFile = new FileWriter("movefile.txt");
+            moveFile.write(username + " " + password+"\n");
+        }catch(IOException e){
+            System.out.println("An error occurred");
+        }
+    }
+
+    public void resumeMatch(String username, String password){
+        try {
+            File readFile =new File ("movefile.txt");
+            Scanner in=new Scanner(readFile);
+            in.nextLine();
+            List<Block> blocks=new ArrayList<>();
+            for(int i=0;i<10;i++){
+                int x=Integer.parseInt(in.next());
+                int y=Integer.parseInt(in.next());
+                int w=Integer.parseInt(in.next());
+                int h=Integer.parseInt(in.next());
+                blocks.add(new Block(x,y,w,h));
+            }
+            Block[] blockArray=new Block[blocks.size()];
+            int goal=0;
+            for(int i=0;i<blocks.size();i++){
+                blockArray[i]=blocks.get(i);
+                if(blocks.get(i).getH()==2 && blocks.get(i).getW()==2) goal=i;
+            }
+            Configuration config=new Configuration(blockArray);
+
+            match = new Match(new User(username, password), new Board(5, 4, goal, config.getBlocks()));
+            this.config = config;
+            newMatchView.hide();
+            matchView.render(match);
+            matchView.show();
+            in.close();
+        }catch (IOException e){
+            System.out.println("An error occurred");
+        }
     }
 
     public void selectBlock(int x, int y) {
@@ -73,16 +100,11 @@ public class MatchController extends Controller {
     }
 
     public void moveBlock(Block.Direction dir) {
+
         boolean ok = match.getBoard().move(selBlock, dir);
-        if (ok) {
+        if(ok){
             match.getMoves().push(new Move(selBlock, dir));
             matchView.render(match);
-        }
-        if (match.getBoard().hasWon()) {
-            System.out.println("Won");
-            matchView.hide();
-            winView.show();
-            winView.render(match);
         }
     }
 
@@ -93,10 +115,25 @@ public class MatchController extends Controller {
     }
 
     public void resetBoard() {
-        match.getBoard().reset(config);
+        match.getBoard().clear();
+        match.getBoard().addAll(config.getBlocks());
         match.getMoves().clear();
         match.resetTimer();
         matchView.render(match);
+    }
+
+    public void save(){
+        try{
+           List<Block> state =match.getBoard().getBlocks();
+           for (int i=0;i<state.size();i++){
+               moveFile.write(state.get(i).toString());
+           }
+           moveFile.close();
+
+        }catch(IOException e){
+            System.out.println("An error occurred");
+        }
+
     }
 
     public int getMovesCount() {
@@ -104,25 +141,9 @@ public class MatchController extends Controller {
             return match.getMoves().size();
         return 0;
     }
+    @Override
+    public void update() {
 
-    public void doNextBestMove() {
-        try {
-            Move bestMove = bestMoveFetcher.getNextBestMove(match.getBoard());
-            bestMove.exec();
-            match.getMoves().push(bestMove);
-            matchView.render(match);
-        }
-        catch (Exception e) {
-            System.err.println(e);
-        }
-        finally {
-            if (match.getBoard().hasWon()) {
-                System.out.println("Won");
-                matchView.hide();
-                winView.show();
-                winView.render(match);
-            }
-        }
     }
 
 }
